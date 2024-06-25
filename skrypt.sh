a2c25efc1398274a4e876d4ad1b0565454a1a013
Norbert <dsw52680@student.dsw.edu.pl>
2024-06-25 19:00:36 +0200

    Dodanie logów

--- skrypt.sh
+++ skrypt.sh
@@ -4,3 +4,15 @@ case "$1" in
     echo "Dzis jest: $(date)"
     ;;
 esac
+  --logs|-l)
+    if [ -z "$2" ]; then
+      count=100
+    else
+      count=$2
+    fi
+    for ((i=1; i<=count; i++))
+    do
+      echo "Log $i zosstał stworzony w skrypt.sh z data $(date)" > log$i.txt
+    done
+    ;;
+esac
