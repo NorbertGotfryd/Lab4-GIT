992951b13b9416a9f31a68036941dd900573a868
Norbert <dsw52680@student.dsw.edu.pl>
2024-06-25 19:08:45 +0200

    Add --help option

--- skrypt.sh
+++ skrypt.sh
@@ -4,3 +4,10 @@ case "$1" in
     echo "Dzis jest: $(date)"
     ;;
 esac
+  --help|-h)
+    echo "Mozliwe opcje:"
+    echo "--data, -d: Dzis jest's date"
+    echo "--logs, -l [number]: Create log files"
+    echo "--help, -h: Display this help message"
+    ;;
+esac
